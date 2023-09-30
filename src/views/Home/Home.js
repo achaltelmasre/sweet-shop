d1fc525b2ea11aeb769959762cc50daaf43e4b78
achaltelmasre <achaltelmasre@gmail.com>
2023-09-30 19:25:30 +0530

    add config file

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import {  faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import {  faShop } from '@fortawesome/free-solid-svg-icons';
+
+import Config from './../../utils/config.json';
+
 
 
 
 function MyComponent() {
   return (
-<>
-    <div className="container">
-      <button type="button" class="btn btn-primary p-3">Primary</button>
-      <p>This is a <FontAwesomeIcon icon={faHeart} /> icon.</p>
+  <>
+      <div className='container' style={{backgroundColor: Config.theme.accentColor}}>
+        <h1 className='text-center display-1 ' style={{color: Config.theme.primaryColor}}>{Config.title} <FontAwesomeIcon icon={ faShop} style={{color: "#063689",}} /> </h1>
+        <p className='text-center h2'>{Config.description} <FontAwesomeIcon icon={  faLocationDot} style={{color: "#e8bd11",}} /></p>
 
-      
+        <h2>Sweets</h2>
+        {
+            Config.services.map((serviceData, index)=>{
+                const {title,img, description, price} = serviceData
+                return(
+                    <div className='card m-3' key={index}>
+                    <div className='card-body'>
 
-    </div>
+                    <h3 className='card-title h1' style={{color: Config.theme.secondaryColor}}>{title}</h3>
+                    
+                         <img  src={img} height={200} width={300} className='p-2 rounded-2'/>
+                         <p className='description p-2'>{description}</p>
+                    <p className='card-text'>Price: {price}</p>
+                    </div>
+                    </div>
+                )
+            })
+        }
+        
+        </div>
  </>  
   );
 }
